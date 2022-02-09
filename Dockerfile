628c5f0b548e8c9abfdb0d0fbbb9fa832a395fb6
Micky <ad312@pm.me>
2022-02-09 23:26:37 +1100

    add database download capability

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,13 @@ FROM python:slim
 COPY . /app
 WORKDIR /app
 
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && apt-get install -y sqlite3 cron && rm -rf /var/lib/apt/lists/*
+
 RUN pip install -r requirements.txt --no-cache-dir
+
+ADD backupDB /etc/cron.d/backupDB
+RUN chmod 0600 /etc/cron.d/backupDB
 
 EXPOSE 80
 
--- starlink/main.py
+++ starlink/main.py
@@ -1,6 +1,8 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, render_template, send_file, url_for, current_app
 )
+from threading import Thread
+import schedule, time
 
 # App imports
 from starlink.firmware import getFirmwareData
@@ -12,13 +14,17 @@ bp = Blueprint('main', __name__)
 def index():
     # Get firmware data for the past 5 dishy entries
     listDict = getFirmwareData(listType="dishy", range=5)
-    
     return render_template('main/index.html', listDict=listDict)
 
 # View for external links
 @bp.route('/info')
 def info():
     return render_template('main/info.html')
+
+# View to download database schema/data
+@bp.route('/database')
+def database():
+    return send_file("static/links/databaseBackup.sql", as_attachment=True)
 
 # 404 page not found error
 @bp.app_errorhandler(404)
