dd3a99972c431a0e1aa23b559f580409580aa759
Micky <ad312@pm.me>
2022-05-29 18:32:05 +1000

    add whitespace check

--- starlink/speedtest.py
+++ starlink/speedtest.py
@@ -107,7 +107,7 @@ def add():
 
     if request.method == 'POST':
         url = request.form['url']
-        if re.search('^https:\/\/www.speedtest.net\/result\/.*$', url): # If url is valid
+        if re.search('^https://www.speedtest.net/result/.\S*$', url): # If url is valid
             if re.search('png', url): # If an image was picked up, get the id and convert to ordinary url
                     url = url.replace(".png", "")
             dbCheck = db.execute('SELECT EXISTS (SELECT 1 FROM speedtests WHERE url = ? LIMIT 1)', (url,)).fetchone()[0]
