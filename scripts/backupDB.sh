8fc16d7bb520aefb900d7aef4b189301679a0b8f
Micky <ad312@pm.me>
2022-06-23 15:21:05 +0300

    fix database backup script

--- scripts/backupDB.sh
+++ scripts/backupDB.sh
@@ -1,5 +1,10 @@
-sqlite3 /app/instance/starlink.db ".schema firmware speedtests" > schema.sql
-sqlite3 /app/instance/starlink.db ".dump firmware speedtests" > dump.sql
+sqlite3 /app/instance/starlink.db ".schema firmware" > schema.sql
+sqlite3 /app/instance/starlink.db ".schema speedtests" >> schema.sql
+
+sqlite3 /app/instance/starlink.db ".dump firmware" > dump.sql
+sqlite3 /app/instance/starlink.db ".dump speedtests" >> dump.sql
+
 grep -vx -f schema.sql dump.sql > databaseBackup.sql
 mv databaseBackup.sql /app/starlink/static/databaseBackup.sql
+
 rm schema.sql dump.sql
