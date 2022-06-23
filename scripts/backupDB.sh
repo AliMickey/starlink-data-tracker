37c7d66f8a825da4f2010c1dd09f9b59392d88a2
Micky <ad312@pm.me>
2022-06-23 15:29:20 +0300

    add perms fix

--- scripts/backupDB.sh
+++ scripts/backupDB.sh
@@ -5,6 +5,7 @@ sqlite3 /app/instance/starlink.db ".dump firmware" > dump.sql
 sqlite3 /app/instance/starlink.db ".dump speedtests" >> dump.sql
 
 grep -vx -f schema.sql dump.sql > databaseBackup.sql
+chmod 755 databaseBackup.sql
 mv databaseBackup.sql /app/starlink/static/databaseBackup.sql
 
 rm schema.sql dump.sql
