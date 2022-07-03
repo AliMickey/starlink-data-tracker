7c9042e9f1505f2dfcd842c4c802e283a0fa4b73
Micky <ad312@pm.me>
2022-07-04 00:56:42 +0300

    change db backup location

--- scripts/backupDB.sh
+++ scripts/backupDB.sh
@@ -6,6 +6,6 @@ sqlite3 /app/instance/starlink.db ".dump speedtests" >> dump.sql
 
 grep -vx -f schema.sql dump.sql > databaseBackup.sql
 chmod 755 databaseBackup.sql
-mv databaseBackup.sql /app/starlink/static/databaseBackup.sql
+mv databaseBackup.sql /app/starlink/static/other/databaseBackup.sql
 
 rm schema.sql dump.sql
