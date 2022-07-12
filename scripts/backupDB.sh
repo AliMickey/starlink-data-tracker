6c6b4078e93183b7e21ba81964776c62a4fd64db
Micky <ad312@pm.me>
2022-07-12 17:35:13 +0300

    purge speedtest extra data

--- scripts/backupDB.sh
+++ scripts/backupDB.sh
@@ -1,8 +1,8 @@
 sqlite3 /app/instance/starlink.db ".schema firmware" > schema.sql
-sqlite3 /app/instance/starlink.db ".schema speedtests" >> schema.sql
+# sqlite3 /app/instance/starlink.db ".schema speedtests" >> schema.sql
 
 sqlite3 /app/instance/starlink.db ".dump firmware" > dump.sql
-sqlite3 /app/instance/starlink.db ".dump speedtests" >> dump.sql
+# sqlite3 /app/instance/starlink.db ".dump speedtests" >> dump.sql
 
 grep -vx -f schema.sql dump.sql > databaseBackup.sql
 chmod 755 databaseBackup.sql
