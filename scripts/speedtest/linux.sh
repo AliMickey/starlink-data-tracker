5a56dc51eb5a5a00300bcdf70309f5e774c3d612
Micky <60691199+AliMickey@users.noreply.github.com>
2023-03-11 12:32:41 +0000

    Update linux.sh

--- scripts/speedtest/linux.sh
+++ scripts/speedtest/linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 speedtestUrl=$(./speedtest -f json | jq -r '.result.url')
 apiKey=()
-curl -X POST --form "api-key=$apiKey" --form "url=$speedtestUrl" https://starlinktrack.com/speedtests/add
+curl -X POST --form "api-key=$apiKey" --form "urls=$speedtestUrl" https://starlinktrack.com/speedtests/add
