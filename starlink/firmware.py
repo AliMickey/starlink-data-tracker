49f8839ea9095f4b4416d46c22604dbde299d518
Micky <60691199+AliMickey@users.noreply.github.com>
2023-05-05 14:53:45 +0100

    remove -prod from router fw regex

--- starlink/firmware.py
+++ starlink/firmware.py
@@ -87,7 +87,7 @@ def add():
 
     if request.method == 'POST':
         listDetails = {'dishy': '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}(.uterm.release)$',
-                        'router': '^(\d{4}[.]\d{2}[.]\d{1,2}[.](mr)\d+(-prod))$', 
+                        'router': '^(\d{4}[.]\d{2}[.]\d{1,2}[.](mr)\d+)$', 
                         'app': '^\d{4}[.]\d{2}[.]\d{1}$',
                         'web': '^\d{1}[.]\d{1}[.]\d{1,2}$',
                         'hardware': ''}
