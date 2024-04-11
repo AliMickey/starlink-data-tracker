cfd7c6ca403972a0e8edc1eb15b8d4fc4adf3487
Micky <60691199+AliMickey@users.noreply.github.com>
2024-04-11 13:20:43 +1000

    fix structure change

--- scripts/backupDB.sh
+++ scripts/backupDB.sh
@@ -1,8 +1,8 @@
-sqlite3 /app/instance/starlink.db ".schema firmware" > schema.sql
-sqlite3 /app/instance/starlink.db ".dump firmware" > dump.sql
+sqlite3 /app/app/starlink.db ".schema firmware" > schema.sql
+sqlite3 /app/app/starlink.db ".dump firmware" > dump.sql
 
 grep -vx -f schema.sql dump.sql > databaseBackup.sql
 chmod 755 databaseBackup.sql
-mv databaseBackup.sql /app/starlink/static/other/databaseBackup.sql
+mv databaseBackup.sql /app/app/static/other/databaseBackup.sql
 
 rm schema.sql dump.sql
