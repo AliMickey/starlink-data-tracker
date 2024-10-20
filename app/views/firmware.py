8a9fd687585255b834ddf3086fa101a9ebe99a4f
Micky <60691199+AliMickey@users.noreply.github.com>
2024-10-20 11:05:29 +1100

    make MR case insensitive for new format

--- app/views/firmware.py
+++ app/views/firmware.py
@@ -86,7 +86,7 @@ def add():
 
     if request.method == 'POST':
         listDetails = {'dishy': '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.uterm(_catapult)?_manifest\.release$',
-                        'router': '^(\d{4}[.]\d{2}[.]\d{1,2}[.](mr)\d+)$', 
+                        'router': '^(\d{4}[.]\d{2}[.]\d{1,2}[.](?i:mr)\d+)$', 
                         'app': '^\d{4}[.]\d{2}[.]\d{1}$',
                         'web': '^\d{1}[.]\d{1}[.]\d{1,2}$',
                         'hardware': ''}
